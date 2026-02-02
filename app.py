76ae179a274393767f4f1f1e8ccc7f11829f2b2e
Mateus Torres <mmtorres230726@gmail.com>
2026-02-02 13:23:38 -0300

    adding migrations and the user table to my db

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
 
 class Product(db.Model):
-    
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     price_in_cents = db.Column(db.Integer, nullable=False) # R$ 1.00 = 100
@@ -19,7 +26,7 @@ class Product(db.Model):
 def get_products():
 
     products = Product.query.all()
-
+ 
     return jsonify([{
         "id": product.id,
         "name": product.name,
@@ -28,20 +35,63 @@ def get_products():
     } for product in products])
     
 
+@app.route("/api/products/<int:product_id>", methods=["GET"])
+def get_product_details(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        return jsonify({
+            "id": product.id,
+            "name": product.name,
+            "price": f"R$ {product.price_in_cents/100:.2f}",
+            "description": product.description
+        }), 200
+    return jsonify({"error": "Product not found"}), 404
+
+
 @app.route("/api/products/add", methods=["POST"])
 def add_product():
     data = request.json
+    if "name" in data and "price_in_cents" in data:
+        product = Product(
+            name=data["name"], 
+            price_in_cents=data["price_in_cents"], 
+            description=data.get("description", "")
+        )
+        db.session.add(product)
+        db.session.commit()
 
-    product = Product(
-        name=data["name"], 
-        price_in_cents=data["price_in_cents"], 
-        description=data.get("description", "")
-    )
+        return jsonify({"message": "Product added successfully"}), 201
+    
+    return jsonify({"error": "Invalid Product data"}), 400
 
-    db.session.add(product)
-    db.session.commit()
 
-    return "Product added successfully", 201
+@app.route("/api/products/delete/<int:product_id>", methods=["DELETE"])
+def delete_product(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        db.session.delete(product)
+        db.session.commit()
+        return jsonify({"message": "Product deleted successfully"}), 200
+    return jsonify({"error": "Product not found"}), 404
+
+
+@app.route("/api/products/update/<int:product_id>", methods=["PUT"])
+def update_product(product_id):
+    data = request.json
+
+    if "name" in data and "price_in_cents" in data:
+        product = Product.query.get(product_id)
+
+        if product:
+            product.name = data["name"]
+            product.price_in_cents = data["price_in_cents"]
+            product.description = data.get("description", "")
+            db.session.commit()
+            return jsonify({"message": "Product updated successfully"}), 200
+
+        return jsonify({"error": "Product not found"}), 404
+
+    return jsonify({"error": "Invalid Product data"}), 400
 
 
 if __name__ == '__main__':
